dab825d72a743c68bec0104c9cf58856acdf9822
Phani2603 <2320030451@klh.edu.in>
2026-01-25 21:37:52 +0530

    feat: icon issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -181,12 +181,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon.ico" }
+      { url: "/cflo1.png", sizes: "32x32", type: "image/png" },
+      { url: "/cflo1.png", sizes: "16x16", type: "image/png" },
+      { url: "/cflo1.png" }
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+      { url: "/cflo1.png", sizes: "180x180", type: "image/png" }
     ],
     other: [
       {
